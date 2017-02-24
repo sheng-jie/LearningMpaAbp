9afee498f98728db34a23524b4ce50c71c20c3e9
jeffrey <ysjshengjie@live.cn>
2017-02-24 13:44:43 +0800

    1、修复BootstrapTable分页出错问题。 2、Ef生成Sql脚本输出到调试窗口

--- LearningMpaAbp.EntityFramework/EntityFramework/LearningMpaAbpDbContext.cs
+++ LearningMpaAbp.EntityFramework/EntityFramework/LearningMpaAbpDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using System.Data.Common;
 using System.Data.Entity;
+using System.Diagnostics;
 using Abp.Zero.EntityFramework;
 using LearningMpaAbp.Authorization.Roles;
 using LearningMpaAbp.MultiTenancy;
@@ -10,37 +11,47 @@ namespace LearningMpaAbp.EntityFramework
 {
     public class LearningMpaAbpDbContext : AbpZeroDbContext<Tenant, Role, User>
     {
-        //TODO: Define an IDbSet for your Entities...
-
-        public IDbSet<Task> Tasks { get; set; }
-
-
         /* NOTE: 
          *   Setting "Default" to base class helps us when working migration commands on Package Manager Console.
          *   But it may cause problems when working Migrate.exe of EF. If you will apply migrations on command line, do not
          *   pass connection string name to base classes. ABP works either way.
          */
+
         public LearningMpaAbpDbContext()
             : base("Default")
         {
-
+            //调试时输出EF执行sql到VS的输出窗口
+            Database.Log = sql => Debug.Write(sql);
+            //输出EF执行sql到log文件
+            //Database.Log = sql => Logger.Log(LogSeverity.Info, sql);
         }
 
         /* NOTE:
          *   This constructor is used by ABP to pass connection string defined in LearningMpaAbpDataModule.PreInitialize.
          *   Notice that, actually you will not directly create an instance of LearningMpaAbpDbContext since ABP automatically handles it.
          */
+
         public LearningMpaAbpDbContext(string nameOrConnectionString)
             : base(nameOrConnectionString)
         {
-
+            //调试时输出EF执行sql到VS的输出窗口
+            Database.Log = sql => Debug.Write(sql);
+            //输出EF执行sql到log文件
+            //Database.Log = sql => Logger.Log(LogSeverity.Info, sql);
         }
 
         //This constructor is used in tests
         public LearningMpaAbpDbContext(DbConnection connection)
             : base(connection, true)
         {
-
+            //调试时输出EF执行sql到VS的输出窗口
+            Database.Log = sql => Debug.Write(sql);
+            //输出EF执行sql到log文件
+            //Database.Log = sql => Logger.Log(LogSeverity.Info, sql);
         }
+
+        //TODO: Define an IDbSet for your Entities...
+
+        public IDbSet<Task> Tasks { get; set; }
     }
 }
